b7d846d05ad47a2597368a5c73768b756590004a
zakaria <zakaria.afyf00@gmail.com>
2023-03-17 00:13:54 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#incude <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Description: Write a C program that prints the line.
+ * puts():a command to output a string 
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}  
