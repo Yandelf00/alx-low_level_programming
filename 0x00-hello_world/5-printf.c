87ee9fc78c7af977fdb41495f5de01a929983ce5
zakaria <zakaria.afyf00@gmail.com>
2023-03-17 01:40:19 +0100

    id

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - Entry point
  *
  * Description: Write a C program that prints
- * The printf() is a library function to send formatted output to the screen
+ * The printf() is a library function to send formatted output to the screen.
  *
- * Return : Always 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
