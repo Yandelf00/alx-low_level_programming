b294afa60390b11abc4f280880080080c25dae01
zakaria <zakaria.afyf00@gmail.com>
2023-03-18 01:36:12 +0100

    print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+	char LETTER = 'A';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	while (LETTER <= 'Z')
+	{
+		putchar(LETTER);
+		LETTER;
+	}
+	putchar('\n');
+	return (0);
+}
