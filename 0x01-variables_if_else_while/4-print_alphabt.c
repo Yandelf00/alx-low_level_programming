af41bbcc039da97b8bd7bd1b8a44e9e765e2f2c0
zakaria <zakaria.afyf00@gmail.com>
2023-03-18 01:48:12 +0100

    idk

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			letter++;
+			continue;
+		}
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
