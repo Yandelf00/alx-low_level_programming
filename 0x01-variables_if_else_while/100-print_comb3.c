e8ba72f175cbdcdf897f84492aebb36b220b2ce8
zakaria <zakaria.afyf00@gmail.com>
2023-03-18 02:37:36 +0100

    idk

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	do {
+		int j = 49;
+		do {
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 56 && j == 57)
+				{
+
+				} else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+			j++;
+		} while (j < 58);
+		i++;
+	} while (i < 57);
+	putchar('\n');
+	return (0);
+}
