5342e4d7157605c80ee9ad8a7750d914f81ef301
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-06 02:20:07 +0000

    rec

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *sqrtt - a function that is going to help find the sqrt.
+ *@n: the int we want to find its sqrt.
+ *@y: another in initialized to zero to find n sqrt.
+ *Return: returns n sqrt.
+ */
+int sqrtt(int n, int y)
+{
+	if (y * y == n)
+	{
+		return (y);
+	}
+	else if (y * y < n)
+	{
+		return (help(n, ++y));
+	}
+	return (-1);
+}
+/**
+ *_sqrt_recursion - a function that calculates sqrt of an int.
+ *@n:an int.	
+ *Return: returns the sqrt of n.
+ */
+int _sqrt_recursion(int n)
+{
+    return (sqrtt(n, 0));
+}
+
