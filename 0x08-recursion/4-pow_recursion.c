cb7727e2cb4268edb14007b67e87af0a47aac0c2
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-06 01:54:40 +0000

    power rec

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_pow_recursion - a function that calculates the power of an int.
+ *@x:an int that we want to calculate its power.
+ *@y:an second int that is the power of x.
+ *Return: returns the power of x per y.
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	if (y > 1)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
