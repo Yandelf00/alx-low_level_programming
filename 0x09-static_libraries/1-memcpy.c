d1bfcce9dc5a3e0d312fd15639c3d8bd7b046ff4
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-07 16:05:35 +0000

    static

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_memcpy - a function that copies bytes from memory area to another one.
+ *@dest:pointer
+ *@src:another pointer
+ *@n:bytes of the memory
+ *Return: Returns a pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+for (i = 0; i < n ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strchr - function that locates a characater in a string.
+ *@s:pointer character
+ *@c:a normal character
+ *Return: Returns a pointer
+*/
+char *_strchr(char *s, char c)
+{
+char *ps = s;
+while (*ps)
+{
+if (*ps == c)
+{
+return (ps);
+}
+ps++;
+}
+if (!c)
+{
+return (ps);
+}
+return (0);
+}
