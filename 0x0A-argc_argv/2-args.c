3884cf9a9513b086f1fc023a2ce841e139b70b22
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-10 22:24:11 +0000

    argstwo

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - this is the main function.
+ *@argc: the count.
+ *@argv: the array that contains the elements.
+ *Return: 0 for success.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i <= argc)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
