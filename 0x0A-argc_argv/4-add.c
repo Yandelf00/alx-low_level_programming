406076ffb01c8e17d95add2cafac694e9cd6f8e9
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-10 22:57:33 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *main - this is the main function.
+ *@argc: the count.
+ *@argv: the array that contains the elements.
+ *Return: 0 for success and 1 one of args isn't int.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	return (0);
+}
