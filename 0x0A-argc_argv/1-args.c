f4567dfdfa1e09b41514e6e963aba1096c25eba7
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-10 22:08:53 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - this is the main function.
+ *@argc: contains the number of elements.
+ *@argv: the array that contains the elements.
+ *Return: 0 for success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
