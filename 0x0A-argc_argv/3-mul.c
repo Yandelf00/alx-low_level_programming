a3c78e73e726fff9f2fa2553180280fd738be2c1
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-10 22:40:05 +0000

    multi

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - this is the main function.
+ *@argc: the count.
+ *@argv: the array that contains the elements.
+ *Return: 0 for success and 1 if there not three args.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]))
+	}
+	return (0);
+}
