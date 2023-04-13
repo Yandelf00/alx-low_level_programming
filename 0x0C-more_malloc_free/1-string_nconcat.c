9e7e5630d0db01d0f9f2babe726568e3a1022110
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-13 16:52:41 +0000

    idk

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -50,10 +50,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n; j++, i++)
 	{
-		ptr[j + l1] = s2[j];
+		ptr[i] = s2[j];
 	}
-	ptr[n + l1] = '\0';
+	ptr[i] = '\0';
 	return (ptr);
 }
