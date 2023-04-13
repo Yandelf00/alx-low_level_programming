8ae58d1c54dfd867494a76d234ce0bb71fc581b7
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-13 16:42:14 +0000

    idk

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = l2;
 	}
-	ptr = (char *)malloc((l1 + n + 1) * sizeof(char));
+	ptr = (char *) malloc((l1 + n + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
 		return (NULL);
