7616037d9ab9b5197e0b07e4748c7719d78cfb92
Yandelf00 <zakaria.afyf00@gmail.com>
2023-04-20 17:24:08 +0000

    printsitall

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints it all
+ * @format: list of the types of parameters passed
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, m = 0;
+	char *arg;
+
+	va_start(valist, format);
+
+	while (*format)
+	{
+		if (m)
+			printf(", ");
+		switch (*format)
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double));
+			break;
+		case 's':
+			arg = va_arg(valist, char *);
+			if (arg)
+			{
+				printf("%s", arg);
+				break;
+			}
+			printf("%p", arg);
+			break;
+		default:
+			m = 0;
+			format++;
+			continue;
+		}
+		m = 1;
+		format++;
+	}
+	printf("\n");
+	va_end(valist);
+}
