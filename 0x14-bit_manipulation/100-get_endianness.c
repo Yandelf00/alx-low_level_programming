1133a496901c6accc08277ecea50fbad8614bcf9
Yandelf00 <zakaria.afyf00@gmail.com>
2023-07-07 00:19:17 +0100

    endiannes

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,15 +2,21 @@
 
 /**
   * get_endianness -  function that checks the endianness.
+  * Return: 0 if big endian, 1 if little endian
   */
 
 int get_endianness(void)
 {
-    int value = 1;
-    char ptr = (char)&value;
+int value = 1;
+char ptr = (char)&value;
 
-    if (*ptr == 1)
-        return (1);
-    else
-        return (0);
+if (*ptr == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
 }
