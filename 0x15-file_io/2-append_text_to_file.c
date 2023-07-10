e4ff7dd1746c25f058c22cb372078be1d6af4d57
Yandelf00 <zakaria.afyf00@gmail.com>
2023-07-10 22:49:16 +0100

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
